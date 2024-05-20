07d8e6cf87fb076560ea56d4e02f8e76b1aa586b
mani627 <mani8754209@gmail.com>
2024-05-20 15:08:06 +0530

    final

--- zip_dragdrop/src/App.jsx
+++ zip_dragdrop/src/App.jsx
@@ -20,64 +20,6 @@ const mockTables = [
       { column_id: "2_2", name: "Disease", column_data_type: "String" },
     ],
   },
-  {
-    id: "3",
-    name: "Associates",
-    columns: [
-      { column_id: "3_1", name: "AssociatesName", column_data_type: "String" },
-      { column_id: "3_2", name: "AssociatesAge", column_data_type: "String" },
-    ],
-  },
-  {
-    id: "4",
-    name: "Contract Workers",
-    columns: [
-      {
-        column_id: "4_1",
-        name: "Contract WorkersName",
-        column_data_type: "String",
-      },
-      {
-        column_id: "4_2",
-        name: "Contract WorkersAge",
-        column_data_type: "String",
-      },
-      { column_id: "4_3", name: "Contract Period", column_data_type: "String" },
-      { column_id: "4_4", name: "Joined Date", column_data_type: "String" },
-      { column_id: "4_5", name: "Role", column_data_type: "String" },
-      { column_id: "4_6", name: "Location", column_data_type: "String" },
-    ],
-  },
-  {
-    id: "5",
-    name: "Securities",
-    columns: [
-      { column_id: "5_1", name: "SecuritiesName", column_data_type: "String" },
-      { column_id: "5_2", name: "SecuritiesAge", column_data_type: "String" },
-      { column_id: "5_3", name: "Field Name", column_data_type: "String" },
-      { column_id: "5_4", name: "Place on Demand", column_data_type: "String" },
-    ],
-  },
-  {
-    id: "6",
-    name: "Buisness Team",
-    columns: [
-      { column_id: "6_1", name: "Name", column_data_type: "String" },
-      { column_id: "6_2", name: "YOE", column_data_type: "String" },
-      { column_id: "6_3", name: "Company Name", column_data_type: "String" },
-      { column_id: "6_4", name: "Contribution", column_data_type: "String" },
-    ],
-  },
-  {
-    id: "7",
-    name: "HR Teams",
-    columns: [
-      { column_id: "7_1", name: "Name", column_data_type: "String" },
-      { column_id: "7_2", name: "Age", column_data_type: "String" },
-      { column_id: "7_3", name: "Location", column_data_type: "String" },
-      { column_id: "7_4", name: "YOJ", column_data_type: "String" },
-    ],
-  },
 ];
 
 function App() {
@@ -95,19 +37,22 @@ function App() {
   };
 
   const handleDrop = (e) => {
-    if (e?.target?.tagName === "LI" || e?.target?.tagName === "SPAN") {
+   
+    if (e?.target?.tagName === "LI"||e?.target?.tagName === "SPAN") {
     } else {
       e.preventDefault();
       const tableInGrid = tablesInGrid.find(
         (table) => table.id === draggedTableId
       );
       if (tableInGrid) {
+      
+
         let innerMove = tablesInGrid.map((item, index) => {
           if (item.id === draggedTableId) {
             return {
               ...item,
               position: {
-                x: e.clientX - e.target.offsetLeft ,
+                x: e.clientX - e.target.offsetLeft - 300,
                 y: e.clientY - e.target.offsetTop,
               },
             };
@@ -125,7 +70,7 @@ function App() {
           {
             ...newTable,
             position: {
-              x: e.clientX - e.target.offsetLeft ,
+              x: e.clientX - e.target.offsetLeft - 400,
               y: e.clientY - e.target.offsetTop,
             },
           },
@@ -190,9 +135,6 @@ function App() {
           const toTable = tablesInGrid.find(
             (table) => table.id === conn.to.tableId
           );
-          {
-            console.log(fromTable, toTable);
-          }
           if (fromTable && toTable) {
             const fromColumn = fromTable.columns.find(
               (col) => col.column_id === conn.from.columnId
@@ -205,7 +147,7 @@ function App() {
             return (
               <line
                 key={index}
-                x1={fromTable.position.x + 100}
+                x1={fromTable.position.x + 400}
                 y1={fromTable.position.y + 70}
                 x2={toTable.position.x + 300}
                 y2={toTable.position.y + 70}
